5934013b0bdb3b5afa6a6eb5bfeadcedab1821cf
fallen0818 <nbumacod14@gmail.com>
2025-04-30 13:52:18 +0800

    Add kwh202504 to Joint_groupby.sql and update WHERE clause for filtering

--- Joint_groupby.sql
+++ Joint_groupby.sql
@@ -2,9 +2,10 @@ SELECT
     c.barangay,
     FORMAT(SUM(u.kwh202501), 0) AS total_kwh202501, 
     FORMAT(SUM(u.kwh202502), 0) AS total_kwh202502, 
-    FORMAT(SUM(u.kwh202503), 0) AS total_kwh202503
+    FORMAT(SUM(u.kwh202503), 0) AS total_kwh202503,
+    FORMAT(SUM(u.kwh202504), 0) AS total_kwh202504
 FROM tblcustomer c
 JOIN tblkwh u ON c.customerid = u.customerid
-WHERE Town LIKE '%agno%'
+WHERE Town LIKE '%alaminos%'
 GROUP BY c.barangay
 ORDER BY c.barangay ASC;
